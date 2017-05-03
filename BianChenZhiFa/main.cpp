2959bcce03116cdf2ce8acdfa3bf62ffc420ba54
顾晅 <guxuan@hecom.cn>
2017-05-03 14:21:30 +0800

    Initial Commit

---
+++ BianChenZhiFa/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BianChenZhiFa
+//
+//  Created by guxuan on 03/05/2017.
+//  Copyright © 2017 gu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
