a1f13f6acf5f58e752cbf28d9de82ce96599e62e
xuangu <guxuan324@gmail.com>
2017-05-11 22:01:33 +0800

    新编12_Print1ToMaxOfNDigits的increment方法

--- BianChenZhiFa/12_Print1ToMaxOfNDigits.cpp
+++ BianChenZhiFa/12_Print1ToMaxOfNDigits.cpp
@@ -20,40 +20,69 @@ void printOneToMaxOfNDigits(const int n) {
     memset(number, '0', n);
     number[n] = '\0';
     
-    while (!increment(number)) {
+    while (increment(number)) {
         printNumber(number);
     }
     
     delete []number;
 }
 
+//bool increment(char *number) {
+//    int carryOnAtIndex = 0;
+//    bool isOverflow = false;
+//    int lenOfNum = (int)strlen(number) - 1;
+//    
+//    for (int i = lenOfNum; i >= 0; i--) {
+//        int bitNumAtIndex = number[i] - '0' + carryOnAtIndex;
+//        if (i == lenOfNum) {
+//            bitNumAtIndex++;
+//        }
+//        
+//        // 判断是否溢出
+//        if (bitNumAtIndex > 9) {
+//            if (i == 0) {
+//                isOverflow = true;
+//            } else {
+//                carryOnAtIndex = 1;
+//                bitNumAtIndex -= 10;
+//                number[i] = '0' + bitNumAtIndex;
+//            }
+//        } else {
+//            number[i] = bitNumAtIndex + '0';
+//            break;
+//        }
+//    }
+//    
+//    return isOverflow;
+//    
+//}
+
 bool increment(char *number) {
-    int carryOnAtIndex = 0;
-    bool isOverflow = false;
+    
     int lenOfNum = (int)strlen(number) - 1;
     
     for (int i = lenOfNum; i >= 0; i--) {
-        int bitNumAtIndex = number[i] - '0' + carryOnAtIndex;
-        if (i == lenOfNum) {
-            bitNumAtIndex++;
+        int bitNumAtCurrentIndex = number[i] - '0';
+        
+        bitNumAtCurrentIndex++;
+        
+        int carryDigit = 0;
+        if (bitNumAtCurrentIndex > 9) {
+            if (i == 0) {
+                return false;
+            }
+            carryDigit = 1;
+            bitNumAtCurrentIndex -= 10;
         }
         
-        // 判断是否溢出
-        if (bitNumAtIndex > 9) {
-            if (i == 0) {
-                isOverflow = true;
-            } else {
-                carryOnAtIndex = 1;
-                bitNumAtIndex -= 10;
-                number[i] = '0' + bitNumAtIndex;
-            }
-        } else {
-            number[i] = bitNumAtIndex + '0';
-            break;
+        number[i] = bitNumAtCurrentIndex + '0';
+        
+        if (carryDigit == 0) {
+            break ;
         }
     }
     
-    return isOverflow;
+    return true;
     
 }
 
