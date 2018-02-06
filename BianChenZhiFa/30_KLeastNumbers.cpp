d0b61d458fa2152079abfdd2e9e629e8092ffe49
guxuan <guxuan@hecom.cn>
2018-02-06 13:22:18 +0800

    文件提交

---
+++ BianChenZhiFa/30_KLeastNumbers.cpp
@@ -0,0 +1,9 @@
+//
+//  30_KLeastNumbers.cpp
+//  BianChenZhiFa
+//
+//  Created by guxuan on 16/06/2017.
+//  Copyright © 2017 gu. All rights reserved.
+//
+
+#include "30_KLeastNumbers.hpp"
---
+++ BianChenZhiFa/30_KLeastNumbers.hpp
@@ -0,0 +1,14 @@
+//
+//  30_KLeastNumbers.hpp
+//  BianChenZhiFa
+//
+//  Created by guxuan on 16/06/2017.
+//  Copyright © 2017 gu. All rights reserved.
+//
+
+#ifndef _0_KLeastNumbers_hpp
+#define _0_KLeastNumbers_hpp
+
+#include <stdio.h>
+
+#endif /* _0_KLeastNumbers_hpp */
